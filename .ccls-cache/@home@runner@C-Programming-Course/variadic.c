322db36c0d8b191df1759c0f7a1797855072aa9d
Mwakanemela <102220067+Mwakanemela@users.noreply.github.com>
2024-06-07 08:58:10 +0000

    A simple variadic function example

---
+++ .ccls-cache/@home@runner@C-Programming-Course/variadic.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+float average(int num, ...);
+
+int main(void) {
+  
+  float average_age = average(3, 4, 5, 6);
+  printf("Average age of 3 people is %f\n", average_age);
+  return 0;
+}
+
+float average(int num, ...) {
+  va_list ap;
+
+  float sum = 0.0;
+
+  int i;
+
+  va_start(ap, num);
+
+  for (i = 0; i < num; i++) {
+      sum += va_arg(ap, int);
+  }
+
+  return (float) sum / num;
+}
